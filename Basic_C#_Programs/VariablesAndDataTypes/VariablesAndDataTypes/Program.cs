d1dd3b68f1b595e73cb9a24e063e630125dac019
the3zero3 <thedini23@gmail.com>
2025-04-16 14:03:34 -0700

    Added initial project files

--- Basic_C#_Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ Basic_C#_Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -11,17 +11,17 @@ namespace VariablesAndDataTypes
         static void Main(string[] args)
         {
             // This is a simple program
-            //Console.WriteLine("What is your name?");
-            //string yourName = Console.ReadLine();
-            //Console.WriteLine("Your name is: " + yourName);
-            //Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
+            Console.ReadLine();
 
-            //Console.WriteLine("What is your favorite number?");
-            //string favoriteNumber = Console.ReadLine();
-            //int favNum = Convert.ToInt32(favoriteNumber);
-            //int total = favNum + 5;
-            //Console.WriteLine("Your favorite number plus 5 is: " + total);
-            //Console.ReadLine();
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum + 5;
+            Console.WriteLine("Your favorite number plus 5 is: " + total);
+            Console.ReadLine();
 
             bool isStudying = false;
             byte hoursWorked = 42;
