aef38944f1d9d750c167b84485d85c6b707ec3d5
the3zero3 <thedini23@gmail.com>
2025-04-29 10:20:27 -0700

    Added initial project files

---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    // Inherits the Person class
+    public class Employee : Person
+    {
+        // Overrides the SayName() method from the Person class
+        public override void SayName()
+        {
+            // Writes the employee's name to the console
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates a new Employee object
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            // Calls the SayName() method from the Employee object
+            employee.SayName();
+            // Keeps the console window open after the program has run
+            Console.ReadLine();
+        }
+    }
+}
