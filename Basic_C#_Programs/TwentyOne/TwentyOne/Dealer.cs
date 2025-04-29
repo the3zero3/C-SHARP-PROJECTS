90d38e754657c383c84481cf55b22224a9aab648
the3zero3 <thedini23@gmail.com>
2025-04-29 10:38:48 -0700

    Updated project files

--- Basic_C#_Programs/TwentyOne/TwentyOne/Dealer.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Dealer.cs
@@ -12,7 +12,7 @@ namespace TwentyOne
         public Deck Deck { get; set; }
         public int Balance { get; set; }
 
-        public void Deal (List<Card> Hand)
+        public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
             Console.WriteLine(Deck.Cards.First().ToString() + "\n");
--- Basic_C#_Programs/TwentyOne/TwentyOne/Game.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Game.cs
@@ -12,7 +12,7 @@ namespace TwentyOne
         public string Name { get; set; }
         public string Dealer { get; set; }
 
-        public abstract void Play(); // Any class inheriting this must has some sort of implementation of this method
+        public abstract void Play(); // Any class inheriting this class must have some sort of implementation of this method
 
         public virtual void ListPlayers() // Virtual method - default implementation can be overridden by the inheriting class
         {
