d458d46acc2ce8357ef7956ac763973c61782680
the3zero3 <thedini23@gmail.com>
2025-05-15 14:06:21 -0700

    Updated project files

--- Basic_C#_Programs/TwentyOne/Casino/obj/Debug/net9.0/Casino.AssemblyInfo.cs
+++ Basic_C#_Programs/TwentyOne/Casino/obj/Debug/net9.0/Casino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Casino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e07bd3c5a147a441e652c54177c5b07081161a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8197e3e1196614fb4fb8f5abf7e0aa994806de80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Casino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Casino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Basic_C#_Programs/TwentyOne/TwentyOne/obj/Debug/net9.0/TwentyOne.AssemblyInfo.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/obj/Debug/net9.0/TwentyOne.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©  2025")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e07bd3c5a147a441e652c54177c5b07081161a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8197e3e1196614fb4fb8f5abf7e0aa994806de80")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
